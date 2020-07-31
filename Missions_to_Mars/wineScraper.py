6bc4dc353a87fb5e7fe925900fa9bfc2a5f76033
Mark Pasricha <markp862@gmail.com>
2020-07-31 16:11:37 -0400

    push

--- Missions_to_Mars/wineScraper.py
+++ Missions_to_Mars/wineScraper.py
@@ -1,6 +1,6 @@
 from splinter import Browser 
 from bs4 import BeautifulSoup 
-
+import pandas as pd
 executable_path = {'executable_path': 'chromedriver.exe'}
 browser = Browser('chrome', **executable_path, headless=False)
 
@@ -15,28 +15,32 @@ wineTypes = soup.find_all("h3", class_="m-0")
 wineList = []
 
 for wine in range(len(wineTypes)): 
-	wineDict = {}
-
 	
 	browser.find_by_css("h3.m-0 a")[wine].click()
 
-	rows = browser.find_by_tag("td[valign=top]")
-	
-	for row in rows:
-		wineType = browser.find_by_css("h1")[1].text
-		#wineDict["wine type"]: wineType
-		print(wineType)
-		print(row.text)
-	# for row in range(len(rows)): 
-	# 	wineType = browser.find_by_css("h1")[1].text
-	# 	wineDict["wine type"]: wineType
+	# rows = browser.find_by_css("td[valign=top]")
+	rows = browser.find_by_css("tr")
 
-	# 	rowData = browser.find_by_tag("td")[row].text
-	# 	print(wineType)
-	# 	print(rowData)
+	wineDict = {}
+	for row in range(len(rows)):
+		
+
+		wineType = browser.find_by_css("h1")[1].text
+		rowHeader  = browser.find_by_tag("h5")[row].text
+		rowData = browser.find_by_css("td[valign=top]")[row].text
+
+		wineDict["type"] = wineType 
+		wineDict[f"{rowHeader}"] = rowData
+		wineList.append(wineDict)
+
+		print(wineDict)
 
 	
 	browser.back()
+
+wine_df = pd.DataFrame(wineList)
+wine_df.to_csv("Data.csv")
+print(wine_df)
 
 
 
